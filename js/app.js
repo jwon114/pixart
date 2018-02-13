45cf60052cfc4f3ac40850c1de4d32984e666712
jwon114 <jthgwong@gmail.com>
2018-02-13 14:24:28 +1100

    change color on enter key press - commit 2

--- js/app.js
+++ js/app.js
@@ -2,6 +2,13 @@ $('.color-input-button').on('click', function(event) {
 	event.preventDefault();
 	var colorInput = $('.color-input').val();
 	if (colorInput !== '') {
-		$('.brush-box').css('background-color', colorInput)
+		$('.brush-box').css('background-color', colorInput);
+	}
+})
+
+$('.color-input').keypress(function(event) {
+	var colorInput = $('.color-input').val();
+	if (event.which === 13 && colorInput !== '') {
+		$('.brush-box').css('background-color', colorInput);
 	}
 })
